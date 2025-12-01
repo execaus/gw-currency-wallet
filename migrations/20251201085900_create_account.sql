998d91e4166550e4181b3bb1a09ba951891dcccc
execaus <execaus@gmail.com>
2025-12-01 14:00:53 +0500

    Добавляет миграцию на создание аккаунта

---
+++ migrations/20251201085900_create_account.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE app.account (
+    email VARCHAR(255) PRIMARY KEY
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS app.account;
+-- +goose StatementEnd
